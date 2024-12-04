00cc028b26d9b1902cae1f4fc65072a16c0623c6
KavithaR14 <129185821+KavithaR14@users.noreply.github.com>
2024-12-04 16:45:33 +0530

    Add files via upload

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Hero.css'; // Create and style this CSS file for hero styling
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="hero-content">
+                <button className="primary">empty</button>
+                <h1 className="hero-title-srds">SRDS</h1>
+                <h1 className="hero-title-transmogrify">TRANSMOGRIFY</h1>
+                <p>At SRDS, we empower the underprivileged through our education, skilling, and livelihood programs...</p>
+                <button className="button-primary">GET INVOLVED</button>
+            </div>
+            <div className="hero-image">
+               
+            </div>
+        </section>
+    );
+};
+
+
+
+
+
+
+
+
+export default Hero;
